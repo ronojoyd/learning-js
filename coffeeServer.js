160ca9f22bf452a818326978bae072a12bf8dfc5
ronojoyd <ron2020d@gmail.com>
2024-03-02 23:23:00 -0500

    Created an HTTP server that returns a coffee shop website

---
+++ coffeeServer.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    fs.readFile('./coffee/index.html', (err, data) => {
+        if (err) {
+            res.writeHead(500, { 'Content-Type': 'text/plain'});
+            res.end(`Internal Server Error: ${err}`);
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/html'});
+            res.end(data);
+        }
+
+    })
+});
+
+server.listen(3000, '127.0.0.1', () => {
+    console.log("Server is running successfully.");
+})
